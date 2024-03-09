b493bfd48de949b59e622c99eaccbb58f44b0738
VinnDev <suryapratamaidn@gmail.com>
2024-03-10 05:02:48 +0700

    update

--- commands/Music/Charts.js
+++ commands/Music/Charts.js
@@ -5,7 +5,7 @@ const Canvas = require("@napi-rs/canvas");
 
 const fs = require("node:fs");
 const path = require("node:path");
-const fontsPath = path.resolve("assets/assets/fonts/Rubik-Regular.ttf");
+const fontsPath = path.resolve("assets/fonts/Rubik-Regular.ttf");
 Canvas.GlobalFonts.register(fs.readFileSync(fontsPath));
 
 module.exports = {
