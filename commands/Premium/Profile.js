b2cf63ec5215372f058eabc27b393cded3350b4e
VinnDev <suryapratamaidn@gmail.com>
2024-03-10 08:55:32 +0700

    update Profile.js

--- commands/Premium/Profile.js
+++ commands/Premium/Profile.js
@@ -54,7 +54,7 @@ module.exports = {
 
         ctx.fillStyle = '#000001';
         ctx.globalAlpha = 0.5;
-        ctx.fillRect(250, 20, /*ctx.measureText(username.length).length +*/ 200, 200);
+        ctx.fillRect(250, 20, /*ctx.measureText(username.length).length +*/ 250, 50);
         ctx.globalAlpha = 1;
 
         ctx.font = 'bold 55px Rubik';
