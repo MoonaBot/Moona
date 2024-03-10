bc20c8fc48e42ef66195186f85894ae3d52df3a7
VinnDev <suryapratamaidn@gmail.com>
2024-03-10 23:05:37 +0700

    update app-command.js

--- app-command.js
+++ app-command.js
@@ -157,7 +157,7 @@ const delay =  require("node:timers/promises").setTimeout;
       let guildId = args[1];
       console.info(`Deploy mode: guild (${guildId})`);
 
-      await rest.put(Routes.applicationGuildCommands(client.id, guildId), { body: commands }).catch(e=>console.info(e.errors['9'][0]));
+      await rest.put(Routes.applicationGuildCommands(client.id, guildId), { body: commands }).catch(e=>console.info(e.rawErrors.errors['9']));
 
       console.info(`Shared commands may take 3-5 seconds to arrive.`);
       break;
