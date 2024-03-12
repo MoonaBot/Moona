19d14663cdca1750c8874a70ec8d111579c1ad95
VinnDev <suryapratamaidn@gmail.com>
2024-03-12 20:51:45 +0700

    update loadPlugin.js

--- handlers/loadPlugin.js
+++ handlers/loadPlugin.js
@@ -1,9 +1,9 @@
 const YouTubeMusic = require("ytmusic-api").default;
-const ytm = new YouTubeMusic();
 
 const { white, green } = require("chalk");
 
-module.exports = async () => {
-    await ytm.initialize();
+module.exports = async (client) => {
+    client.ytm = new YouTubeMusic();
+    await client.ytm.initialize();
     //console.log(white('[') + green('INFO') + white('] ') + green('Plugin ') + white('Handlers') + green(' Loaded!'));
 };
