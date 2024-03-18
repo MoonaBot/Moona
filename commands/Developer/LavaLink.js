2eee0a6f30371f352d04239964f42eaa8ab58a05
VinnDev <suryapratamaidn@gmail.com>
2024-03-18 20:29:50 +0700

    update LavaLink.js

--- commands/Developer/LavaLink.js
+++ commands/Developer/LavaLink.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
-const ButtonPage = require('../../utils/../../utils/ButtonPage.js');
+const ButtonPage = require('../../utils/ButtonPage.js');
 const prettyBytes = require("pretty-bytes");
 const { request } = require("undici");
 
